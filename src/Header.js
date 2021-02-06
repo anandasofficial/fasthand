7c6f335798ba6853491b96206853395417133bf6
anandasofficial <gitthubofficial@gmail.com>
2021-02-06 18:43:41 +0530

    css commits

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from "@material-ui/core";
 function Header() {
   return (
     <div className='header'>
      
-       <img className = "header_logo"  
-       src = "https://www.searchpng.com/wp-content/uploads/2019/01/Amazon-Logo.png" alt = ""/>
-    <div className = "header_search">
-    <input className = "header_searchInput" type = "text" />
-
-    </div> 
-    <div className = "header_nav">
-    <div className = "header_option">
-    <span className = 'header_optionLineOn'>Hello World</span>
-    </div>
-    </div>
+       <img className = "header_icon"  
+       src = "http://assets.stickpng.com/images/580b57fcd9996e24bc43c513.png" alt = ""/>
+      <div className = 'header_center'>
+        <input type = "text"/>
+        <SearchIcon/>
+        
+        
+      </div>
+      <div className = 'header_right'>
+        <p>Become a host</p>
+        <LanguageIcon/>
+        <ExpandMoreIcon/>
+        <Avatar/>
+      </div>
     </div>
   );
 }
