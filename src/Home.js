47b45a2d0ed57ea2cc06bc969fee6b9983ce3b19
anandasofficial <gitthubofficial@gmail.com>
2021-02-06 19:22:53 +0530

    new commit

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Banner from './Banner';
 import  './Home.css'
 function Home() {
   return (
     <div className="home">
-      
+        <h1>Home Here!</h1>
+      <Banner/>
        
     </div>
   );
